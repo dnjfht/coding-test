67d6e91978f6b399c8d432f22bc89170357a995d
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2023-02-28 03:58:04 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/README.md
+++ 프로그래머스/lv0/120830. 양꼬치/README.md
@@ -1,10 +1,10 @@
 # [level 0] 양꼬치 - 120830 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120830?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120830) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.03 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,8 +1,11 @@
 function solution(n, k) {
-    if(n >= 10) {
-        return 12000 * n + 2000 * (k - Math.floor(n/10));
-    } else {
-        return 12000 * n + 2000 * k;
-    }
+    var answer = 0;
     
+    if(n < 10) {
+        answer = 12000 * n + 2000 * k;
+        return answer;
+    } else if (n >= 10) {
+        answer = 12000 * n + (k - Math.floor(n / 10)) * 2000;
+        return answer;
+    }
 }
