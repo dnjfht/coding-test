40cea2de831245a06c2b0ccba47952f973134489
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2023-02-28 04:22:57 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/README.md
+++ 프로그래머스/lv0/120829. 각도기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 각도기 - 120829 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829) 
 
 ### 성능 요약
 
-메모리: 33.3 MB, 시간: 0.03 ms
+메모리: 33.1 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,11 +1,11 @@
 function solution(angle) {
     if(0 < angle && angle < 90) {
-        return 1;        
-    } else if(90 === angle) {
+        return 1;
+    } else if (angle === 90) {
         return 2;
-    } else if (90 < angle && angle < 180) {
+    } else if (90 < angle && angle < 180 ) {
         return 3;
-    } else {
+    } else if (angle === 180) {
         return 4;
     }
 }
