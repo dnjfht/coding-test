6e953641a354b822726936480a05ee35f0627b13
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2023-03-19 22:47:59 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,9 +1,11 @@
 function solution(array, height) {
-   var answer = [];
-   for ( let i = 0; i < array.length; i++ ) {
-       if (array[i] > height) {
-           answer.push(array[i]);
-       }
-   }
-   return answer.length;
+    let result = [];
+    
+    for (let i = 0; i <= array.length-1; i++) {
+        if(array[i] > height){
+            result.push(array[i]);
+        }
+    }
+    
+    return result.length;
 }
