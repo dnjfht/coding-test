cd770b264a5d033beeb72eedbf8c5cf09851f5c0
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2023-03-20 00:15:26 +0900

    Update 머쓱이보다 키 큰 사람.js
    두 가지 방법으로 코테 풀기

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,11 +1,14 @@
 function solution(array, height) {
-    let result = [];
+//     let result = [];
     
-    for (let i = 0; i <= array.length-1; i++) {
-        if(array[i] > height){
-            result.push(array[i]);
-        }
-    }
+//     for (let i = 0; i <= array.length-1; i++) {
+//         if(array[i] > height){
+//             result.push(array[i]);
+//         }
+//     }
     
+//     return result.length;
+    
+    let result = array.filter(kid => kid > height);
     return result.length;
 }
