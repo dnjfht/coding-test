5173d8ffd8e96fb9bc9dba8662f2cd75ef32ade0
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2023-03-23 06:30:26 +0900

    Update 평균 구하기.js

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,3 +1,7 @@
+// function solution(arr) {
+//     return arr.reduce((sum, num) => sum += num ,0) / arr.length;
+// }
+
 function solution(arr) {
-    return arr.reduce((sum, num) => sum += num ,0) / arr.length;
+    return arr.reduce((sum, num, _, array) => sum + num / array.length ,0);
 }
