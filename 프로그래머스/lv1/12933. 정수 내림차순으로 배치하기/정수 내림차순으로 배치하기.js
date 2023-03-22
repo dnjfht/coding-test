de0c162cd7cc0476a8eb00e55c6a50d91a3ebb4f
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2023-03-23 06:26:51 +0900

    Update 정수 내림차순으로 배치하기.js

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,3 +1,5 @@
 function solution(n) {
     return parseInt(String(n).split("").map((num)=>Number(num)).sort((a,b)=>b-a).join(""));
+    // 숫자를 배열에 담아주기 위한 과정 => String을 사용하여 문자열로 변환 => split을 사용하여 숫자를 분리시켜 배열에 담아줌.
+    // => map()을 통하여 숫자로 형 변환을 해주면 됨(Number 메소드 사용) 
 }
