db4b89695a3270781c6e83ba085365ebaf0b5648
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2025-02-14 11:45:22 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(myString) {
+    // var answer = '';
+    // for(let x of myString) {
+    //   answer += x.toLowerCase();
+    // }
+    // return answer;
+    
+    return myString.toLowerCase();
+}
