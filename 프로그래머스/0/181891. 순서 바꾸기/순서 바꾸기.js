bb3f07dddf3cb12095f9cfd892f34779b6dd1477
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2025-02-17 20:22:53 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -0,0 +1,11 @@
+function solution(num_list, n) {
+//     let bigIdxArr = [];
+//     let smallIdxArr = [];
+    
+//     for(let i = 0; i < num_list.length; i++) i < n ? smallIdxArr.push(num_list[i]) : bigIdxArr.push(num_list[i]);
+//     return [...bigIdxArr, ...smallIdxArr];
+    
+    const smallIdxArr = num_list.slice(0,n);
+    const bigIdxArr = num_list.slice(n);
+    return [...bigIdxArr, ...smallIdxArr];
+}
