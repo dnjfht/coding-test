a9101526efd7a99a84303f3ae22c7871c0598659
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2025-02-17 19:22:51 +0900

    [level 0] Title: 5명씩, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -0,0 +1,9 @@
+function solution(names) {
+    // let answer = [];
+    // for(let i = 0; i < names.length; i++) {
+    //     if(i % 5 === 0) answer.push(names[i]);
+    // }
+    // return answer;
+    
+    return names.filter((_, idx) => idx % 5 === 0);
+}
