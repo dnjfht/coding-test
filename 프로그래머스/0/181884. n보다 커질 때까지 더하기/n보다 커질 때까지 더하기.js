1c46349a3919d7454c48c23230794135b3b61586
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2025-07-25 20:05:16 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,22 +1,6 @@
 function solution(numbers, n) {
-// let answer = 0;
+    // var answer = 0;
+    // return answer;
+    return numbers.reduce((arr, num) => arr <= n ? arr + num : arr, 0);
     
-//     for(let i = 0; i < numbers.length; i++) {
-//         if(answer <= n) {
-//             answer += numbers[i];
-//         }
-//     }
-     
-//     for (const num of numbers) {
-//         answer += num;
-//         if (answer > n) {
-//             return answer;
-//         }
-//     }
-    
-//     return answer;
-    
-    return numbers.reduce((acc, crt) => {
-      return acc > n ?  acc : acc + crt;
-    }, 0)
 }
