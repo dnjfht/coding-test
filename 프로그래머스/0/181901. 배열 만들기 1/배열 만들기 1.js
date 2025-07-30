9dafb19dc5c6136f6ec6a3eda9707e29777dfe57
Yoo Seungmin <117057638+dnjfht@users.noreply.github.com>
2025-07-30 13:02:30 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.25 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -0,0 +1,10 @@
+function solution(n, k) {
+    var answer = [];
+    
+    for(let i = 0; i <= n; i+=k){
+        if(i !== 0) {
+          answer.push(i);  
+        }
+    }
+    return answer;
+}
